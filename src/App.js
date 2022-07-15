0ab520d1587910e58c814f5464cd05de327f6ac7
Yousef Ayman <106432204+Yussif20@users.noreply.github.com>
2022-07-15 14:09:16 +0200

    adding the css rules and the button

--- src/App.js
+++ src/App.js
@@ -31,6 +31,9 @@ class App extends React.Component {
       <div className="app">
         <div className="card">
           <h1 className="heading">{advice}</h1>
+          <button onClick={this.fetchAdvice} className="button">
+            <span>Give me an advice!</span>
+          </button>
         </div>
       </div>
     );
